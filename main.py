5e5b92b8980540b94c449cc3e33c58e38311aae1
Mikolaj Hawryluk <hawrylukmikolaj@gmail.com>
2023-06-08 02:55:15 +0200

    dolewej_poprawka

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 def format_list_right(input_list):
     formatted_string = ""
     max_lengths = [max(map(len, map(str, col))) for col in zip(*input_list)]
+    #print(max_lengths)
     dlugosc = len(input_list)
     indeks = 1
     formatted_string += "["
     for row in input_list:
         formatted_string += "["
         for i, num in enumerate(row):
-            formatted_string += f"{str(num):<{max_lengths[i]}}"
+            cos = ","
+            #formatted_string += str(num)
+            #formatted_string += f"{str(num):<{max_lengths[i]}}"
+            #fmt = '{:<' + f'{max_lengths[i]+1}' + '}'
+            #formatted_string += fmt.format(str(num) + ',')
             if i != len(row) - 1:
-                formatted_string += ", "
+                fmt = '{:<' + f'{max_lengths[i]+1}' + '}'
+                formatted_string += fmt.format(str(num) + ',')
+                formatted_string += " "
+            else:
+                formatted_string += f"{str(num):<{max_lengths[i]}}"
+        #formatted_string = formatted_string[:-1]
         if indeks != dlugosc:
             formatted_string += "],\n "
         else:
