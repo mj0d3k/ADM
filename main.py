a910b8dfe6e28cefcc63e698f82986052df53415
Mikolaj Hawryluk <hawrylukmikolaj@gmail.com>
2023-06-12 11:58:46 +0200

    testy_lewa

--- main.py
+++ main.py
@@ -40,8 +40,8 @@ def format_list_left(input_list):
         indeks += 1
     return formatted_string
 
-l = [[1, 2, 10, 150], [10, 2, 2, 2], [1, 120, 1, 100]]
-output = format_list_right(l)
+l = [[1, 2, 10, 150], [10, 2, 1000, 2], [1, 120, 1, 1000]]
+output = format_list_left(l)
 print(output)
 
 # testy
@@ -64,4 +64,21 @@ result = """[[  656, 231,    231,  50],
  [   76,   6,      2, 992]]"""
 assert format_list_right(example) == result
 
+example = [[19900, 5, 120, 1150], [155, 1, 12000, 8992], [3, 12, 455, 100]]
+result = """[[19900, 5,  120,   1150],
+ [155,   1,  12000, 8992],
+ [3,     12, 455,   100 ]]"""
+assert format_list_left(example) == result
+
+example = [[656, 231, 231, 50], [1445, 6, 121, 992]]
+result = """[[656,  231, 231, 50 ],
+ [1445, 6,   121, 992]]"""
+assert format_list_left(example) == result
+
+example = [[656, 231, 231, 50], [1445, 6, 1, 992], [12132, 6, 242334, 992], [76, 6, 2, 992]]
+result = """[[656,   231, 231,    50 ],
+ [1445,  6,   1,      992],
+ [12132, 6,   242334, 992],
+ [76,    6,   2,      992]]"""
+assert format_list_left(example) == result
 
